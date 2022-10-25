3182b94dd009dd1508b0d9ca64635aef9ea6d065
zidanalif <zidanalifmaulana1@gmail.com>
2022-10-25 14:42:50 +0700

    models

--- app/Models/AbsensiKetidakhadiran.php
+++ app/Models/AbsensiKetidakhadiran.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class AbsensiKetidakhadiran extends Model
 {
     use HasFactory;
+
+    public function karyawan()
+    {
+        return $this->belongsTo(Karyawan::class);
+    }
+
+    public function cuti()
+    {
+        return $this->belongsTo(Cuti::class);
+    }
+
+    public function periode()
+    {
+        return $this->belongsTo(Periode::class);
+    }
 }
--- app/Models/Cuti.php
+++ app/Models/Cuti.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cuti extends Model
 {
     use HasFactory;
+
+    public function absensi_ketidakhadiran()
+    {
+        return $this->hasMany(AbsensiKetidakhadiran::class);
+    }
 }
