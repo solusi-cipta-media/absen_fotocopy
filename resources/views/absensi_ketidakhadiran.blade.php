6856d33d6dc28686a947849ec130dbce3f429160
zidanalif <zidanalifmaulana1@gmail.com>
2022-10-25 14:58:23 +0700

    revisi modal

--- resources/views/absensi_ketidakhadiran.blade.php
+++ resources/views/absensi_ketidakhadiran.blade.php
@@ -74,34 +74,34 @@
         </div>
 
         <!-- Normal Modal -->
-<div class="modal" id="modal_bukti" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenter" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="block block-rounded shadow-none mb-0">
-                <div class="block-header block-header-default">
-                    <h3 class="block-title">Terms &amp; Conditions</h3>
-                    <div class="block-options">
-                        <button type="button" class="btn-block-option" data-bs-dismiss="modal" aria-label="Close">
-                            <i class="fa fa-times"></i>
-                        </button>
+        <div class="modal" id="modal_bukti" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenter" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="block block-rounded shadow-none mb-0">
+                        <div class="block-header block-header-default">
+                            <h3 class="block-title">Terms &amp; Conditions</h3>
+                            <div class="block-options">
+                                <button type="button" class="btn-block-option" data-bs-dismiss="modal" aria-label="Close">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="block-content fs-sm" id="body-modal">
+                            <img src="" alt="bukti-pengajuan" class="w-100">
+                        </div>
+                        <div class="block-content block-content-full block-content-sm text-end border-top">
+                            <button type="button" class="btn btn-alt-secondary" data-bs-dismiss="modal">
+                                Close
+                            </button>
+                            <button type="button" class="btn btn-alt-primary" data-bs-dismiss="modal">
+                                Done
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div class="block-content fs-sm" id="body-modal">
-                    <img src="" alt="bukti-pengajuan" class="w-100">
-                </div>
-                <div class="block-content block-content-full block-content-sm text-end border-top">
-                    <button type="button" class="btn btn-alt-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="button" class="btn btn-alt-primary" data-bs-dismiss="modal">
-                        Done
-                    </button>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-<!-- END Normal Modal -->
+        <!-- END Normal Modal -->
 
         <!-- Dynamic Table Responsive -->
     </div>
