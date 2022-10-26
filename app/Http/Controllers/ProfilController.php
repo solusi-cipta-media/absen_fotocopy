a6b885d717e453407adec69bcd957d12c37cdecd
zidanalif <zidanalifmaulana1@gmail.com>
2022-10-26 16:25:18 +0700

    login & profile

---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Facades\Image;
+
+class ProfilController extends Controller
+{
+    public function index()
+    {
+        return view('profil');
+    }
+
+    public function update_user_profile(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email|unique:karyawans,email,'.auth()->user()->id,
+            'nama' => 'required|min:2',
+            'foto' =>  'required|file|mimes:png,jpg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = auth()->user();
+
+        if(File::exists($user->foto)){
+            File::delete($user->foto);
+        }
+
+        $file = $request->file('foto');
+        $name = $file->hashName(); // Generate a unique, random name...
+        $imageName = $name;
+        $img = Image::make($file->path());
+        
+        // resize image
+        $img->fit(400, 400, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save('media/karyawan/'.$imageName);
+
+        $user->foto = 'media/karyawan/'.$imageName;
+        $user->nama = $request->nama;
+        $user->email = $request->email;
+        $user->save();
+
+        return response()->json(['message' => 'Data telah ditambahkan', 'data' => $user],200);
+    }
+
+    public function update_password(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'password' => 'required',
+            'npassword' => 'required|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+        $user = auth()->user();
+        if ( Hash::check($request->password, $user->password)) {
+            $user->password = Hash::make($request->npassword);
+            $user->save();
+            return response()->json(['message' => 'Data telah diupdate'],200); 
+        }
+        return response()->json(['message' => 'Gagal mengubah data'],400);
+    }
+
+    public function update_informasi_pribadi(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'nama' => 'required',
+            'alamat' => 'required',
+            'telepon' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = auth()->user();
+        $user->nama = $request->nama;
+        $user->alamat = $request->alamat;
+        $user->telepon = $request->telepon;
+        $user->save();
+
+        return response()->json(['message' => 'Data telah diupdate'],200); 
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\CutiController;
 use App\Http\Controllers\KaryawanController;
 use App\Http\Controllers\KontrakController;
 use App\Http\Controllers\MesinController;
+use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,77 +20,85 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/login', [AuthController::class, 'login_page'])->name('login.form');
 
-Route::get('profil', function () {
-    return view('profil');
-})->name('profil');
+Route::middleware(['auth', 'supervisor'])->group(function ()
+{
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::get('karyawan', [KaryawanController::class, 'index'])->name('karyawan');
-Route::post('karyawan', [KaryawanController::class, 'store'])->name('karyawan.store');
-Route::get('karyawan/{id}', [KaryawanController::class, 'get'])->name('karyawan.get');
-Route::post('karyawan/{id}', [KaryawanController::class, 'update'])->name('karyawan.update');
-Route::delete('karyawan/{id}', [KaryawanController::class, 'destroy'])->name('karyawan.delete');
+    Route::get('karyawan', [KaryawanController::class, 'index'])->name('karyawan');
+    Route::post('karyawan', [KaryawanController::class, 'store'])->name('karyawan.store');
+    Route::get('karyawan/{id}', [KaryawanController::class, 'get'])->name('karyawan.get');
+    Route::post('karyawan/{id}', [KaryawanController::class, 'update'])->name('karyawan.update');
+    Route::delete('karyawan/{id}', [KaryawanController::class, 'destroy'])->name('karyawan.delete');
 
-Route::get('mesin', [MesinController::class , 'index'])->name('mesin');
-Route::post('mesin', [MesinController::class , 'store'])->name('mesin.add');
-Route::put('mesin/{id}', [MesinController::class , 'update'])->name('mesin.update');
-Route::get('mesin/{id}', [MesinController::class , 'get'])->name('mesin.get');
-Route::delete('mesin/{id}', [MesinController::class , 'destroy'])->name('mesin.delete');
+    Route::get('mesin', [MesinController::class , 'index'])->name('mesin');
+    Route::post('mesin', [MesinController::class , 'store'])->name('mesin.add');
+    Route::put('mesin/{id}', [MesinController::class , 'update'])->name('mesin.update');
+    Route::get('mesin/{id}', [MesinController::class , 'get'])->name('mesin.get');
+    Route::delete('mesin/{id}', [MesinController::class , 'destroy'])->name('mesin.delete');
 
-Route::post('customer/select',[CustomerController::class, 'select'])->name('customer.select');
-Route::get('customer',[CustomerController::class, 'index'])->name('customer');
-Route::post('customer',[CustomerController::class, 'store'])->name('customer.store');
-Route::get('customer/code',[CustomerController::class, 'generateCode'])->name('customer.code');
-Route::get('customer/{id}',[CustomerController::class, 'get'])->name('customer.get');
-Route::post('customer/{id}',[CustomerController::class, 'update'])->name('customer.update');
-Route::delete('customer/{id}',[CustomerController::class, 'destroy'])->name('customer.delete');
-
-
-Route::get('kontrak', [KontrakController::class, 'index'])->name('kontrak');
-Route::post('kontrak', [KontrakController::class, 'store'])->name('kontrak.store');
-Route::get('kontrak/{id}', [KontrakController::class, 'get'])->name('kontrak.get');
-Route::post('kontrak/{id}', [KontrakController::class, 'update'])->name('kontrak.update');
-Route::delete('kontrak/{id}', [KontrakController::class, 'destroy'])->name('kontrak.delete');
-
-Route::get('cuti', [CutiController::class, 'index'])->name('cuti');
-Route::post('cuti', [CutiController::class, 'store'])->name('cuti.store');
-Route::get('cuti/{id}', [CutiController::class, 'get'])->name('cuti.get');
-Route::post('cuti/{id}', [CutiController::class, 'update'])->name('cuti.update');
-Route::delete('cuti/{id}', [CutiController::class, 'destroy'])->name('cuti.delete');
+    Route::post('customer/select',[CustomerController::class, 'select'])->name('customer.select');
+    Route::get('customer',[CustomerController::class, 'index'])->name('customer');
+    Route::post('customer',[CustomerController::class, 'store'])->name('customer.store');
+    Route::get('customer/code',[CustomerController::class, 'generateCode'])->name('customer.code');
+    Route::get('customer/{id}',[CustomerController::class, 'get'])->name('customer.get');
+    Route::post('customer/{id}',[CustomerController::class, 'update'])->name('customer.update');
+    Route::delete('customer/{id}',[CustomerController::class, 'destroy'])->name('customer.delete');
 
 
-Route::get('absensi', function () {
-    return view('absensi');
-})->name('absensi');
+    Route::get('kontrak', [KontrakController::class, 'index'])->name('kontrak');
+    Route::post('kontrak', [KontrakController::class, 'store'])->name('kontrak.store');
+    Route::get('kontrak/{id}', [KontrakController::class, 'get'])->name('kontrak.get');
+    Route::post('kontrak/{id}', [KontrakController::class, 'update'])->name('kontrak.update');
+    Route::delete('kontrak/{id}', [KontrakController::class, 'destroy'])->name('kontrak.delete');
 
-Route::get('absensi_ketidakhadiran', function () {
-    return view('absensi_ketidakhadiran');
-})->name('absensi_ketidakhadiran');
+    Route::get('cuti', [CutiController::class, 'index'])->name('cuti');
+    Route::post('cuti', [CutiController::class, 'store'])->name('cuti.store');
+    Route::get('cuti/{id}', [CutiController::class, 'get'])->name('cuti.get');
+    Route::post('cuti/{id}', [CutiController::class, 'update'])->name('cuti.update');
+    Route::delete('cuti/{id}', [CutiController::class, 'destroy'])->name('cuti.delete');
 
 
-Route::get('periode', function () {
-    return view('periode');
-})->name('periode');
+    Route::get('absensi', function () {
+        return view('absensi');
+    })->name('absensi');
 
-Route::get('overhaul_list', function () {
-    return view('overhaul_list');
-})->name('overhaul_list');
+    Route::get('absensi_ketidakhadiran', function () {
+        return view('absensi_ketidakhadiran');
+    })->name('absensi_ketidakhadiran');
 
-Route::get('overhaul_proses', function () {
-    return view('overhaul_proses');
-})->name('overhaul_proses');
 
-Route::get('cekqr', function () {
-    return view('cekqr');
-})->name('cekqr');
+    Route::get('periode', function () {
+        return view('periode');
+    })->name('periode');
+    
+    Route::get('profil', [ProfilController::class, 'index'])->name('profil');
+    Route::post('profil/user', [ProfilController::class, 'update_user_profile'])->name('profil.user');
+    Route::post('profil/password', [ProfilController::class, 'update_password'])->name('profil.password');
+    Route::post('profil/info', [ProfilController::class, 'update_informasi_pribadi'])->name('profil.informasi');
 
-Route::get('jadwal_spk', function () {
-    return view('jadwal_spk');
-})->name('jadwal_spk');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('overhaul_list', function () {
+        return view('overhaul_list');
+    })->name('overhaul_list');
+
+    Route::get('overhaul_proses', function () {
+        return view('overhaul_proses');
+    })->name('overhaul_proses');
+
+    Route::get('cekqr', function () {
+        return view('cekqr');
+    })->name('cekqr');
+
+    Route::get('jadwal_spk', function () {
+        return view('jadwal_spk');
+    })->name('jadwal_spk');
+});
