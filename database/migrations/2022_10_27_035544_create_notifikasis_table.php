c1c28956d08528f63e0ee5eb041d4053b61f7926
zidanalif <zidanalifmaulana1@gmail.com>
2022-10-27 15:08:23 +0700

    migration

---
+++ database/migrations/2022_10_27_035544_create_notifikasis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifikasis', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kontrak_id');
+            $table->foreign('kontrak_id')->references('id')->on('kontraks')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('pesan');
+            $table->date('tanggal');
+            $table->enum('status',['unread', 'read']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifikasis');
+    }
+};
